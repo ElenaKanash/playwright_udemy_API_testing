5c87bd148fc545cc7596b92f2227c45c8af007cc
ElenaKanash <hird250@gmail.com>
2025-07-31 19:55:10 +0300

    moking tags API

--- tests/api_tests.spec.ts
+++ tests/api_tests.spec.ts
@@ -1,10 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
+  await page.route('https://conduit-api.bondaracademy.com/api/tags',
+    async route => {
+      const tags = {
+    "tags": [
+        "Tag_1",
+        "Tag_2",
+        "Tag_3",
+        "Tag_4"
+    ]
+}
+      await route.fulfill({
+        body: JSON.stringify(tags)
+      });      
+    });    
+
   await page.goto('https://conduit.bondaracademy.com/');
+  await page.waitForTimeout(500) 
 });
 
-test('has title', async ({ page }) => {
-  await page.goto('https://conduit.bondaracademy.com/');
+test('has title', async ({ page }) => {  
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
 });
