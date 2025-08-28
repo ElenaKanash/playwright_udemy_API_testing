94cd59367b7839bfe2fd53d80745b72c3bcbefbe
ElenaKanash <hird250@gmail.com>
2025-08-28 21:26:51 +0300

    add mocking body API

--- tests/api_tests.spec.ts
+++ tests/api_tests.spec.ts
@@ -1,18 +1,36 @@
 import { test, expect } from '@playwright/test';
-import tags from '../test-data/tags.json'
+import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
-  await page.route('*/**/api/tags',
-    async route => {
-      await route.fulfill({
-        body: JSON.stringify(tags)
-      });      
-    });    
+  //modify tags
+  await page.route('*/**/api/tags', async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
+
+  //modify body
+  await page.route('*/**/api/articles*', async (route) => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+
+    responseBody.articles[0].title = 'Mock test title';
+    responseBody.articles[0].description = 'Mock test description';
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody),
+    });
+  });
 
   await page.goto('https://conduit.bondaracademy.com/');
-  await page.waitForTimeout(500) 
+  await page.waitForTimeout(1000);
 });
 
-test('has title', async ({ page }) => {  
+test('Modify tags', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+});
+
+test('Modify body', async ({ page }) => {
+  await expect(page.locator('app-article-list h1').first()).toHaveText('Mock test title');
+  await expect(page.locator('app-article-list p').first()).toHaveText('Mock test description');
 });
